105911799c463ab5bef24c86da6a16f941c12c23
aldiwira <aldiwira.aw@gmail.com>
2020-06-05 16:05:47 +0700

    add color pallates

---
+++ src/config/colors.js
@@ -0,0 +1,9 @@
+module.exports = {
+  colorWhite: '#ffffff',
+  colorBlack: '#000000',
+  colorBlueFlat: '#90CAF9',
+  colorWhiteFlat: '#EFEFEF',
+  colorTwinkleBlue: '#d1d8e0',
+  colorBlueNTSC: '#4b7bec',
+  colorBlackMe: '#151515',
+};
