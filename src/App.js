5fbb48e59e1cd6e279b7fb196400bcdc868ed8f0
aldiwira <aldiwira.aw@gmail.com>
2020-06-06 16:59:29 +0700

    add navigator

--- src/App.js
+++ src/App.js
@@ -1,72 +1,33 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, SafeAreaView, FlatList} from 'react-native';
-import AppHeader from './components/headers';
-import AppInput from './components/inputbar';
+import React from 'react';
+import {Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import MainApp from './screens/Main';
 import colors from './config/colors';
-import AppTask from './components/task';
-import moment from 'moment';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  useEffect(() => {});
-  const [ListOfTask, setListOfTask] = useState([]);
-  const [Task, setTask] = useState(null);
-  const date = moment();
-  const addTask = () => {
-    const task = Task;
-    const payload = {
-      id: ListOfTask.length,
-      value: task,
-      date: date.format('dddd, DD MMMM YYYY'),
-      time: date.format('LT'),
-    };
-    setListOfTask([...ListOfTask, payload]);
-    setTask(null);
-  };
-  const deleteTask = id => {
-    const newList = ListOfTask.slice('id', id);
-    setListOfTask(newList);
-  };
   return (
-    <View style={styles.ViewWrapper}>
-      <View>
-        <AppHeader title="Todo Apps" />
-        <AppInput
-          value={Task}
-          onChange={text => {
-            setTask(text);
-          }}
-          onPress={addTask}
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.colorBlueNTSC,
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name={'Home'}
+          component={MainApp}
+          options={{title: 'Todo Apps'}}
         />
-      </View>
-
-      <SafeAreaView style={styles.ViewScroll}>
-        <FlatList
-          data={ListOfTask}
-          renderItem={({item}) => (
-            <AppTask
-              title={item.value}
-              date={item.date}
-              time={item.time}
-              onPress={() => {
-                deleteTask(item.id);
-              }}
-            />
-          )}
-          contentContainerStyle={{flexGrow: 1}}
-          keyExtractor={item => item.id.toString()}
-        />
-      </SafeAreaView>
-    </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  ViewWrapper: {
-    backgroundColor: colors.colorWhiteFlat,
-  },
-  ViewScroll: {
-    backgroundColor: colors.colorWhiteFlat,
-  },
-});
 
 export default App;
